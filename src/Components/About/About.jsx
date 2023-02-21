f9b21140425c253f89dce627be840fba5f4acecb
David Chen <DavidChen@Davids-iMac.local>
2023-02-21 15:34:03 -0500

    fixed contact page layout with flex

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,19 +4,22 @@ import templeofheaven from '../../Assets/TempleOfHeaven.png'
 
 function About() {
     return (
-        <div className='about'>
-            <h1 className='about-us'>About Us</h1>
+        <div className='about-background'>
+            <div className='about'>
+                <h1 className='about-us'>About Us</h1>
             <div>
                 <img alt="templeofheaven" src={templeofheaven} className="temple-of-heave-image"/>
             </div>
-            <h3 className='about-spacing'>
+                <h3 className='about-spacing'>
                 Locally owned and operated. Owner, Niao Huan Chen, 
                 immigrated to the United States back in the 1980s with a dream of 
-                owning his own business. He started as a waiter and chef in Chinatown, NYC then moved to Lumberton, NC in 1998. There he opened his first business,
-                Lung Wah Chinese Restaurant, then it evolved into Soho Bistro. After 15 years of owning his business in Lumberton,
+                owning his own restaurant. He started his apprenticeship in Chinatown, NYC learning from the best chefs. An opportunity arrived
+                for him to open a restaurant in Lumberton, NC. Having learned all that he could he decieded to persue his dream and moved to Lumberton, NC in 1998.
+                There he opened his first business, Lung Wah Chinese Restaurant, then it evolved into Soho Bistro. After 15 years of owning his business in Lumberton,
                 he moved to Dillion, SC to venture into China Buffet continuing to perfect his skills 
                 and talent as a Chef.
-            </h3>
+                </h3>
+            </div>
         </div>
     )
 }
