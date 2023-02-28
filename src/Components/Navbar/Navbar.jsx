c5dd8a52c45468f156486c3474a8be753924f180
David Chen <DavidChen@Davids-iMac.local>
2023-02-28 15:07:54 -0500

    added mobile button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,47 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    },[]);
+
+    window.addEventListener('resize', showButton);
+
     return (
         <nav>
+            <div onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
             <ul className='nav-links'>
-                <Link className='nav-style' to="/">
+                <Link className='nav-style' to="/" onClick={closeMobileMenu}>
                     <li>Home</li>
                 </Link>
-                <Link className='nav-style' to="/menu">
+                <Link className='nav-style' to="/menu" onClick={closeMobileMenu}>
                     <li>Menu</li>
                 </Link>
-                <Link className='nav-style' to="/about">
+                <Link className='nav-style' to="/about" onClick={closeMobileMenu}>
                     <li>About</li>
                 </Link>
-                <Link className='nav-style' to="/contact">
+                <Link className='nav-style' to="/contact" onClick={closeMobileMenu}>
                     <li>Contact</li>
                 </Link>
             </ul>
