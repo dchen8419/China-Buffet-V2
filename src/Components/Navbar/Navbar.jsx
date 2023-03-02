b362996c7703489abc7c369cb9575a08f3a3e88e
David Chen <DavidChen@Davids-iMac.local>
2023-03-02 09:21:59 -0500

    added navbar button with mobile settings

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -26,27 +26,68 @@ function Navbar() {
 
     window.addEventListener('resize', showButton);
 
+    // return (
+    //     <nav>
+    //         <div onClick={handleClick}>
+    //             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+    //         </div>
+    //         <ul className='nav-links'>
+    //             <Link className='nav-style' to="/" onClick={closeMobileMenu}>
+    //                 <li>Home</li>
+    //             </Link>
+    //             <Link className='nav-style' to="/menu" onClick={closeMobileMenu}>
+    //                 <li>Menu</li>
+    //             </Link>
+    //             <Link className='nav-style' to="/about" onClick={closeMobileMenu}>
+    //                 <li>About</li>
+    //             </Link>
+    //             <Link className='nav-style' to="/contact" onClick={closeMobileMenu}>
+    //                 <li>Contact</li>
+    //             </Link>
+    //         </ul>
+    //         <div>
+    //         {/* {button && <Button buttonStyle='btn--outline'></Button>} */}
+    //         </div>
+    //     </nav>
+    // )
+
     return (
-        <nav>
-            <div onClick={handleClick}>
-                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-            </div>
-            <ul className='nav-links'>
-                <Link className='nav-style' to="/" onClick={closeMobileMenu}>
-                    <li>Home</li>
-                </Link>
-                <Link className='nav-style' to="/menu" onClick={closeMobileMenu}>
-                    <li>Menu</li>
-                </Link>
-                <Link className='nav-style' to="/about" onClick={closeMobileMenu}>
-                    <li>About</li>
-                </Link>
-                <Link className='nav-style' to="/contact" onClick={closeMobileMenu}>
-                    <li>Contact</li>
-                </Link>
-            </ul>
-        </nav>
-    )
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        China Buffet 
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/menu' className='nav-links' onClick={closeMobileMenu}>
+                                Menu
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                    {button}
+                </div>
+            </nav>  
+        </>
+        )
 }
 
 export default Navbar
