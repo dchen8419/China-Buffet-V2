01d254fb7441cea34ddac84e6c94f5528c3fe5fa
David Chen <DavidChen@Davids-iMac.local>
2023-03-30 21:12:02 -0400

    finished all css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,57 +1,33 @@
 import React from 'react'
 import './Footer.css'
-import { Link } from 'react-router-dom'
 
 export const Footer = () => {
     return (
         <div className='footer-container'>
-        <div className='footer-links'>
-            <div className='footer-link-wrapper'>
-                <div className='footer-link-items'>
-                    <h2>About Us</h2>
-                    <Link to="/contact">Careers</Link>
-                </div>
-                <div className='footer-link-items'>
-                    <h2>Contact Us</h2>
-                    <Link to='/contact'>Contact</Link>
-                    <Link to='/contact'>Support</Link>
+            <div className='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                        <a href="https://goo.gl/maps/NpxminUJ5zycHaWL6">
+                            <h2>Address</h2>
+                            <p>208 US-301</p>
+                            <p>Dillon, SC 29536</p>
+                        </a>
+                    </div>
+                    <div className='footer-link-items'>
+                        <a href="tel:843-841-3188">
+                            <h2>Contact Us</h2>
+                            <p>(843) 841-3188</p>
+                        </a>
+                    </div>
                 </div>
             </div>
+            <section className='social-media'>
+                <div className='social-media-wrap'>
+                    <div className='footer-logo'>
+                </div>
+                    <small className='website-rights'>David Chen &copy;{new Date().getFullYear()} Luna Media LLC</small>
+                </div>
+            </section>
         </div>
-        <section className='social-media'>
-            <div className='social-media-wrap'>
-                <div className='footer-logo'>
-                </div>
-                <small className='website-rights'>David Chen &copy;{new Date().getFullYear()} Luna Media LLC | All right reserved | Terms and Conditions | Privacy</small>
-            </div>
-        </section>
-    </div>
     )
 }
-
-// export const Footer = () => {
-//     return (
-//         <div className='footer-container'>
-//             <div>
-//                 <h1>Something</h1>
-//                 <ul>
-//                     <li>One</li>
-//                     <li>Two</li>
-//                 </ul>
-//             </div>
-//             <div>
-//                 <h1>Something else</h1>
-//                 <ul>
-//                     <li>three</li>
-//                     <li>four</li>
-//                 </ul>
-//             </div>
-//             <div>
-//                 <hr />
-//                 <p>
-//                     David Chen &copy;{new Date().getFullYear()} Luna Media LLC | All right reserved | Terms and Conditions | Privacy
-//                 </p>
-//             </div>
-//         </div>
-//     )
-// }
